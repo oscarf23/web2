70e4822c8ba43b5aae60f11bd0eb6b4c45ef9690
oscarf23 <oscarmf23@gmail.com>
2020-05-01 16:51:02 -0500

    almost there!

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    console.log("Hope you like to look at memes, enjoy!");
+
+});
+
+function myFunction() {
+    document.getElementById("learnMore").innerHTML = "The word meme is originated from the ancient Greek word mimema, which means something imitated. In today's generation, a meme is a type of picture or video that is spread from the Internet, often through social media. Memes can come from just about anything, whether it's through sports, your favorite tv show, movies, music, or your own creativity!";}
+
+    $(document).ready(function() { 
+        setTimeout(function() { 
+            $('#button').fadeOut(); 
+     }, 15000); 
+    });
